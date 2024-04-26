7405a64f329167cf31693fa4f56cac81229c7df4
Asim Ali <142142845+asimalizamurani@users.noreply.github.com>
2024-04-26 11:45:53 +0500

    Simple calculator in Php
    This is a simple calculator using php

---
+++ calculator/cal.php
@@ -0,0 +1,12 @@
+<?php
+echo "<h2>" . "The result is : " . "</h2>";
+
+// $sign = $_POST['sign'];
+
+echo "The Addition is : " . $_POST['numOne'] + $_POST['numTwo']."<br>"; 
+echo "The Subtraction is : " . $_POST['numOne'] - $_POST['numTwo'] ."<br>";
+echo "The Multiplication is : " . $_POST['numOne'] * $_POST['numTwo'] ."<br>";
+echo "The Division is : " . $_POST['numOne'] / $_POST['numTwo'] ."<br>";
+
+
+?>  
